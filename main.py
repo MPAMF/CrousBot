47bdc76276fdccbaccaf65dbe5d2df95a26066c6
SchawnnDev <iperskill@gmail.com>
2023-03-08 11:56:39 +0100

    replaced single to double quotes

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class CrousBotClient(discord.Client):
     }
 
     async def on_ready(self):
-        print('Logged on as', self.user)
+        print("Logged on as", self.user)
 
     async def on_message(self, message):
         # don't respond to ourselves
@@ -33,12 +33,12 @@ class CrousBotClient(discord.Client):
 
         content: str = message.content
 
-        if content.startswith('merci mr crous bot'):
-            msg = 'ferme la {0.author.mention}'.format(message)
+        if content.startswith("merci mr crous bot"):
+            msg = "ferme la {0.author.mention}".format(message)
             await message.channel.send(msg)
             return
 
-        if content.startswith('!menu'):
+        if content.startswith("!menu"):
             args = content.split(" ")
             url = self.sites["illkirch"]
 
@@ -47,21 +47,21 @@ class CrousBotClient(discord.Client):
                     url = self.sites[args[1]]
                 else:
                     await message.channel.send(
-                        'Arguments possibles: !menu illkirch/esplanade/paulappel')
+                        "Arguments possibles: !menu illkirch/esplanade/paulappel")
                     return
 
             r = requests.get(url, verify=False)
             soup = BeautifulSoup(r.text, features="html.parser")
 
             date = "Menu du %s" % format_date(datetime.date.today(),
-                                              'EEEE d MMMM y',
-                                              locale='fr_FR')
+                                              "EEEE d MMMM y",
+                                              locale="fr_FR")
 
-            name = self.names[args[1] if len(args) == 2 else 'illkirch']
+            name = self.names[args[1] if len(args) == 2 else "illkirch"]
             msg = ["%s à %s" % (date, name), ""]
 
-            for h in soup.findAll('div', {"class": "menu"}):
-                content = h.find('time', {"class", "menu_date_title"})
+            for h in soup.findAll("div", {"class": "menu"}):
+                content = h.find("time", {"class", "menu_date_title"})
 
                 if len(content) == 0:
                     continue
@@ -71,7 +71,7 @@ class CrousBotClient(discord.Client):
                 if found_date != date:
                     continue
 
-                content = h.find('ul', {"class": "meal_foodies"}).findAll('li')
+                content = h.find("ul", {"class": "meal_foodies"}).findAll("li")
 
                 if content is None or len(content) == 0:
                     continue
@@ -84,13 +84,13 @@ class CrousBotClient(discord.Client):
 
                     title = text[0].get_text()
 
-                    if 'PERSONNELS' in title or 'Origines' in title:
+                    if "PERSONNELS" in title or "Origines" in title:
                         continue
 
                     msg.append(title)
 
                     for li in text[1].contents:
-                        if li.text == '' or li.text == '-':
+                        if li.text == "" or li.text == "-":
                             continue
                         msg.append("• %s" % li.text)
 
@@ -104,7 +104,8 @@ class CrousBotClient(discord.Client):
 
             await message.channel.send("\n".join(msg))
 
+
 intents = discord.Intents.default()
 intents.message_content = True
 client = CrousBotClient(intents=intents)
-client.run(os.getenv('DISCORD_API_KEY'))
+client.run(os.getenv("DISCORD_API_KEY"))
