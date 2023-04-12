42a2dc768897a935557dccc0e69c4918fe821e9c
Vincent WENDLING <vincent@starling-burgers.com>
2023-04-12 09:46:17 +0200

    feat: added public holidays

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import discord
 import requests
 import random
+import datetime
 from babel.dates import format_date
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
@@ -152,6 +153,28 @@ class CrousBotClient(discord.Client):
                     await referenced_message.add_reaction("🐟")
                     # await message.delete() # should we delete the message the author sent ?? 
             return
+
+        if content.startswith('!ferie'):
+            d = datetime.date.today()
+            today = datetime.datetime(d.year, d.month, d.day)
+
+            ferie_metropole      = requests.get(f"https://calendrier.api.gouv.fr/jours-feries/metropole/{d.year}.json").json()
+            ferie_aslace_moselle = requests.get(f"https://calendrier.api.gouv.fr/jours-feries/alsace-moselle/{d.year}.json").json()
+
+            for (date, day) in ferie_aslace_moselle.items():
+                public_holiday = datetime.datetime.strptime(date, "%Y-%m-%d")
+                
+                if public_holiday > today: # next holiday
+                    await message.channel.send(f"Le prochain jour férié pour les bg qui bossent en Alsace-Moselle est le {date} - {day}")
+                    break
+            
+            for (date, day) in ferie_metropole.items():
+                public_holiday = datetime.datetime.strptime(date, "%Y-%m-%d")
+                
+                if public_holiday > today: # next holiday
+                    await message.channel.send(f"Le prochain jour férié pour les crasseux qui bossent autre part est le {date} - {day}")
+                    break
+
 
 intents = discord.Intents.default()
 intents.message_content = True
